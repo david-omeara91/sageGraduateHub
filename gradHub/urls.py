ea67cb63d04715e3a5934446f1c854b9fbbcb586
david-omeara91 <david.omeara@sage.com>
2022-05-15 15:45:21 +0100

    add Auth Token

--- gradHub/urls.py
+++ gradHub/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from gradHubApp import views
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -20,5 +21,6 @@ router.register(r'advice', views.AdviceViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('admin/', admin.site.urls),
 ]

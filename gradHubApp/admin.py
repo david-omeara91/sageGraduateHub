af0f71bf14328a6d29f18a99cb845f66babc5e7e
david-omeara91 <david.omeara@sage.com>
2022-05-15 16:30:16 +0100

    add models for resources

--- gradHubApp/admin.py
+++ gradHubApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Graduate, Event, Resource, Mentor, Question, Answer, Advice
+from .models import (Graduate, Event, Resource, ResourceCategory,
+                     Mentor, Question, Answer, Advice)
 # Register your models here.
 admin.site.register(Graduate)
 admin.site.register(Event)
+admin.site.register(ResourceCategory)
 admin.site.register(Resource)
 admin.site.register(Mentor)
 admin.site.register(Question)
--- gradHubApp/models.py
+++ gradHubApp/models.py
@@ -1,4 +1,5 @@
 from turtle import title
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -18,10 +19,22 @@ class Event(models.Model):
     location=models.CharField(max_length=30)
     date=models.DateField()
 
+class ResourceCategory(models.Model):
+    categoryName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.categoryName
+
 class Resource(models.Model):
     title=models.CharField(max_length=40)
+    imageURL = models.URLField(null=True)
+    description = models.TextField(max_length=200, null=True)
     url=models.URLField()
-    category = models.CharField
+    category = models.CharField(max_length=30,null=True )
+    category = models.ForeignKey(ResourceCategory, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 class Mentor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
